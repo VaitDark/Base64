df5a4f92632871ee2e71e4aca1026258c8a314b7
Raider <Raider@DESKTOP-VQ80KVP>
2021-09-27 20:08:11 +0300

    Добавьте файлы проекта.

---
+++ Base64.cpp
@@ -0,0 +1,93 @@
+﻿// Base64.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include "Table.h"
+
+using namespace std;
+int shift = 0;
+char a;
+int bc[1000];
+int main()
+{
+    ifstream inp("inp.txt");
+    int arr[8]{};
+        //int bc[1000];
+        int  ost, i;
+        int j = 0;
+        i = 0;
+        while (inp.get(a)) {
+        while (a != 0) {
+            ost = a % 2;
+            a /= 2;
+            arr[i] = ost;
+            i++;
+        }
+        i = 0;
+        j = 8;
+        while(!j==0){
+            
+            bc[8+shift - j] = arr[j-1];
+            j--;
+        }
+        shift += 8;
+    }
+        inp.close();
+        ofstream output("output.txt");
+        shift -= 8;
+        for (int i = 0; i < shift + 8; i++) {
+            cout << bc[i];
+        }
+        cout << endl;
+        ost = (shift + 8) % 6;
+        ost = 6 - ost;
+        //ost /= 2;
+        for (int i =0; i <ost; i++) {
+             bc[(shift+8-1)+i]=0;
+        }
+        for (int i = 0; i < shift + 8 + (ost ); i++) {
+            cout << bc[i];
+        }
+        bool b = true;
+        i = 1;
+        int shetchic = 0;
+        int boof[6];
+        while (b) {
+            if (shetchic<=shift+8+ost) {
+                boof[i-1] = bc[shetchic];
+                if (i % 6==0) {
+                    int bfnum;
+                    bfnum = boof[3] * 1;
+                    bfnum += boof[2] * 2;
+                    bfnum += boof[1] * 4;
+                    bfnum += boof[0] * 8;
+                    bfnum += boof[0] * 16;
+                    bfnum += boof[0] * 32;
+                    a= table(bfnum);
+                    output << a;
+                }
+            }
+            else {
+                b = false;
+            }
+            if (i % 6 == 0) {
+                i = 1;
+            }
+            else {
+                i++;
+            }
+            shetchic++;
+        }
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Table.cpp
@@ -0,0 +1,203 @@
+#include "Table.h"
+
+char table(int  bfnum) {
+    char a='*';
+    switch (bfnum)
+    {
+    default:
+        break;
+    case 0:
+        a = 'A';
+        break;
+    case 1:
+        a = 'B';
+        break;
+    case 2:
+        a = 'C';
+        break;
+    case 3:
+        a = 'D';
+        break;
+    case 4:
+        a = 'E';
+        break;
+    case 5:
+        a = 'F';
+        break;
+    case 6:
+        a = 'G';
+        break;
+    case 7:
+        a = 'H';
+        break;
+    case 8:
+        a = 'I';
+        break;
+    case 9:
+        a = 'J';
+        break;
+    case 10:
+        a = 'K';
+        break;
+    case 11:
+        a = 'L';
+        break;
+    case 12:
+        a = 'M';
+        break;
+    case 13:
+        a = 'N';
+        break;
+    case 14:
+        a = 'O';
+        break;
+    case 15:
+        a = 'P';
+        break;
+    case 16:
+        a = 'Q';
+        break;
+    case 17:
+        a = 'R';
+        break;
+    case 18:
+        a = 'S';
+        break;
+    case 19:
+        a = 'T';
+        break;
+    case 20:
+        a = 'U';
+        break;
+    case 21:
+        a = 'V';
+        break;
+    case 22:
+        a = 'W';
+        break;
+    case 23:
+        a = 'X';
+        break;
+    case 24:
+        a = 'Y';
+        break;
+    case 25:
+        a = 'Z';
+        break;
+    case 26:
+        a = 'a';
+        break;
+    case 27:
+        a = 'b';
+        break;
+    case 28:
+        a = 'c';
+        break;
+    case 29:
+        a = 'd';
+        break;
+    case 30:
+        a = 'e';
+        break;
+    case 31:
+        a = 'f';
+        break;
+    case 32:
+        a = 'g';
+        break;
+    case 33:
+        a = 'h';
+        break;
+    case 34:
+        a = 'i';
+        break;
+    case 35:
+        a = 'j';
+        break;
+    case 36:
+        a = 'k';
+        break;
+    case 37:
+        a = 'l';
+        break;
+    case 38:
+        a = 'm';
+        break;
+    case 39:
+        a = 'n';
+        break;
+    case 40:
+        a = 'o';
+        break;
+    case 41:
+        a = 'p';
+        break;
+    case 42:
+        a = 'q';
+        break;
+    case 43:
+        a = 'r';
+        break;
+    case 44:
+        a = 's';
+        break;
+    case 45:
+        a = 't';
+        break;
+    case 46:
+        a = 'u';
+        break;
+    case 47:
+        a = 'v';
+        break;
+    case 48:
+        a = 'w';
+        break;
+    case 49:
+        a = 'x';
+        break;
+    case 50:
+        a = 'y';
+        break;
+    case 51:
+        a = 'z';
+        break;
+    case 52:
+        a = '0';
+        break;
+    case 53:
+        a = '1';
+        break;
+    case 54:
+        a = '2';
+        break;
+    case 55:
+        a = '3';
+        break;
+    case 56:
+        a = '4';
+        break;
+    case 57:
+        a = '5';
+        break;
+    case 58:
+        a = '6';
+        break;
+    case 59:
+        a = '7';
+        break;
+    case 60:
+        a = '8';
+        break;
+    case 61:
+        a = '9';
+        break;
+    case 62:
+        a = '+';
+        break;
+    case 63:
+        a = '/';
+        break;
+    }
+    return a;
+}
